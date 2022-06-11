07992137760b10ff4240d8ffc4ef73e0adbda758
Illusion of control <sergeyskorokhod2@gmail.com>
2022-06-11 19:49:59 +0300

    initial commit

---
+++ src/main/java/org/capricorn/Application.java
@@ -0,0 +1,10 @@
+package org.capricorn;
+
+import org.capricorn.*;
+
+public class Application {
+    public static void main(String[] args) {
+        DownloadManager manager = new DownloadManager();
+        manager.setVisible(true);
+    }
+}
